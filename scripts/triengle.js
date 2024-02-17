5bc97241f2e1e5e8b4252ada8beeab1ba7e26616
Ariful4a <afkgamingno1@gmail.com>
2024-02-17 21:22:03 +0600

    added all new changes item

--- scripts/triengle.js
+++ scripts/triengle.js
@@ -73,14 +73,56 @@ function calculateParallelogramArea(){
 }
 
 
-function calculateRhombusleArea(){
-const d1 = getRomboshd1();
-console.log(d1);
+// ebabe kora jai man paower jonno 
+// function calculateRhombusleArea(){
+// const d1 = getRomboshd1();
+// console.log(d1);
+// }
+
+// function getRomboshd1(){
+//     const romboshInput = document.getElementById('Rhombus-d1');
+//     const romboshText = romboshInput.value;
+//     const d1 = parseFloat(romboshText);
+//     return d1;
+// }
+
+
+// Rombush area 
+// eta improtant ebebe korle sobida
+function calculateRhombusArea(){
+    // input section :1
+    const base = getInputValueById('Rhombus-d1')
+    console.log('Rombus value', base);
+// input section :2
+    const height =getInputValueById('Rhombus-d2');
+    console.log('height er value', height);
+
+    // calculate area 
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    setInnerTextById('Rhombus-Area', area);
 }
 
-function getRomboshd1(){
-    const romboshInput = document.getElementById('Rhombus-d1');
-    const romboshText = romboshInput.value;
-    const d1 = parseFloat(romboshText);
-    return d1;
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputText = inputField.value;
+    const inputValue = parseFloat(inputText);
+    // console.log(inputValue);
+    return inputValue;
+}
+
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
+
+
+// Ellipse-Area 
+// ebabe kora jai claculate shotcat poddoti 
+function calculateEllipseArea(){
+    const ellipsWidth = getInputValueById('Ellipse-width');
+    const ellipsLength = getInputValueById('Ellipse-length');
+    const area = 3.14 * ellipsWidth * ellipsLength;
+    setInnerTextById('Ellipse-Area', area);
 }
